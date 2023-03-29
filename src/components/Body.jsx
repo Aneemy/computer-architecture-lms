c4e7c0432024857b88a1133e01659237b057a991
Aneemy <sasha.singin@gmail.com>
2023-03-29 07:32:53 +0300

    siglesum output logic 1/2

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,9 +2,26 @@ import React, {useEffect, useState} from 'react';
 import SumRow from "./summators/SumRow";
 import './styles/main.css';
 import CurrentSummator from "./summators/CurrentSummator";
-import {TransitionGroup} from "react-transition-group";
+
 const Body = (props) => {
 const [sumToggle,setSumToggle]=useState(false);
+const [leftSum,setLeftSum] = useState(0);
+const [rightSum,setRightSum] = useState(0);
+const [curIt,setCurIt] = useState({out:0,buf:0})
+    function OneOrNull() {
+        var k = 0;
+        if (leftSum === 1)
+            k++
+        if (rightSum === 1)
+            k++
+        if (k===2&&curIt.buf === 1)
+            k++
+        return k;
+    }
+useEffect((()=>{
+    setCurIt({...curIt,out:OneOrNull()})
+}),[rightSum])
+
     if(props.flag){
         return (
             <div className="body">
@@ -13,8 +30,8 @@ const [sumToggle,setSumToggle]=useState(false);
                     <div>
                         <CurrentSummator curSum = {props.curSum}/>
                             <div style={{display:"flex"}}>
-                                <SumRow on = {sumToggle} setOn = {setSumToggle} row={props.binary.first}/>
-                                <SumRow on = {sumToggle} setOn = {setSumToggle} row={props.binary.second}/>
+                                <SumRow  setSumData = {setLeftSum} on = {sumToggle} row={props.binary.first}/>
+                                <SumRow  setSumData = {setRightSum} on = {sumToggle} row={props.binary.second}/>
                             </div>
                         <button  onClick={()=> setSumToggle(!sumToggle)} style={{float:"right"}}>Включить</button>
                     </div>
--- src/components/summators/SumRow.jsx
+++ src/components/summators/SumRow.jsx
@@ -1,33 +1,26 @@
-import React, {useEffect, useLayoutEffect, useState} from 'react';
+import React, {useState} from 'react';
 import SumRowItem from "./SumRowItem";
 import {CSSTransition} from "react-transition-group";
 
 const SumRow = (props) => {
     const [numbers,setNumbers] = useState(props.row);
-    const [iteration,setIteration] = useState(false);
     const [currentIndex,setCurrentIndex] = useState(numbers.length-1)
     function itemIteration(){
         setNumbers(numbers.filter((_,index)=> index !== numbers.length-1))
         setCurrentIndex(currentIndex-1);
-        setIteration(true);
+        props.setSumData(currentIndex)
     }
-    useLayoutEffect(() => {
-        if(props.on&&numbers.length>0){
-            console.log("effect")
-        }
-    }, [props.on,numbers,iteration]);
 
 
     return (
         numbers.length>0 ?
-
         <div className = "sinsum__row">
             {numbers.map((number,index) =>
                 <CSSTransition
                     in={currentIndex===index&&props.on}
                     key={index}
                     classNames="sinsum__item"
-                    timeout={2000}
+                    timeout={1000}
                     onEntered = {()=>itemIteration()}
                     >
                     <SumRowItem key = {index}>{number}</SumRowItem>
@@ -36,7 +29,6 @@ const SumRow = (props) => {
         </div>
             :
             <div>
-                Кончился
             </div>
     );
 };
