039740fe927fe5a3ec50e6abdfe1b2efdcc01caa
Aneemy <sasha.singin@gmail.com>
2023-06-18 21:26:51 +0300

    mocha

--- src/components/dashboard/TestConst.jsx
+++ src/components/dashboard/TestConst.jsx
@@ -1,109 +1,113 @@
-import React, { useEffect, useState } from 'react';
-import Header from '../Header';
-import DbSideBar from './DBSideBar';
-import Body from '../Body';
-import axios from 'axios';
-import { question } from '../../http/user';
+import React, {useEffect, useState} from 'react';
+import Header from "../Header";
+import DbSideBar from "./DBSideBar";
+import Body from "../Body";
+import axios from "axios";
+import {question} from "../../http/user";
+
 
 const TestConst = () => {
-    const [questionList, setQuestionList] = useState(null);
-    const [testList, setTestList] = useState([]);
+    const [questionList,setQuestionList] = useState(null)
+    const [testList,setTestList] = useState([]);
     const [isReady, setIsReady] = useState(false);
-
-    const getQuestionList = async () => {
-        let token = localStorage.getItem("token");
-        try {
-            const response = await axios.get('http://192.168.56.101:8080/teacher/'+token+'/quests');
-            setQuestionList(response.data);
-            console.log(response.data);
-        } catch (e) {
-            alert(e);
-        }
-    };
-
-    const sendTest = async (name, list) => {
-        try {
-            const response = await axios.post('', {
-                name: name,
-                value: list
-            });
-            if (response.status !== '201') {
-                alert('Cool');
-            } else {
-                alert('ad');
-            }
-        } catch (e) {
-            alert(e.message);
-        }
-    };
-
-    useEffect(() => {
-        getQuestionList();
-    }, []);
-
-    const getRequiredQuestion = async (question, setQuestion) => {
-        try {
-            const response = await axios.get('', {
-                question: question
-            });
-            setQuestion(response.data);
-        } catch (e) {
-            alert(e.response.message);
-        }
-    };
-
-    const QuestionItem = ({ question }) => {
-        const [currentQuestion, setCurrentQuestion] = useState({
-            name: '',
-            text: '',
-            pictures: [],
-            options: []
-        });
-
-        const handleRequestQuestion = async () => {
+    const getQuestionList = async () =>{
+        return async () => {
             try {
-                const response = await axios.get('', { question: question });
-                setCurrentQuestion(response.data);
-            } catch (e) {
-                alert(e.response.message);
+                const response = await axios.get("", {})
+                setQuestionList(response.data)
+                console.log(response.data)
             }
-        };
-
-        const prepareTest = () => {
-            if (testList.includes(question)) {
-                setTestList((prevList) => prevList.filter((quest) => quest !== question));
-            } else {
-                setTestList((prevList) => [...prevList, question]);
+            catch (e){
+                alert(e)
             }
-        };
-
-        return (
-            <div onClick={prepareTest}>
-                {question.name}
+        }
+    }
+    const sendTest = async (name,list) =>{
+        try {
+            const response = await axios.post('',{
+                name:name,
+                value:list
+            })
+            if (response.status!=='201') {
+                alert("Cool")
+            }
+            else{
+                alert("ad")
+            }
+        }
+        catch (e) {
+            alert(e.message)
+        }
+    }
+    useEffect(()=>{
+        getQuestionList()
+    },[])
+    const PrintQuestionList = () =>{
+        const getRequiredQuestion = async (question,setQuestion) =>{
+            try {
+                const response = await axios.get("",
+                    {question:question})
+                setQuestion(response.data)
+            }
+            catch (e){
+                alert(e.response.message)
+            }
+        }
+        if (questionList!==null){
+            return(
                 <div>
-                    <span onClick={handleRequestQuestion}>Запросить вопрос</span>
-                    <div>
-                        <span>{currentQuestion.name}</span>
-                        <span>{currentQuestion.text}</span>
-                        <div>
-                            {currentQuestion.pictures.map((picture, index) => (
-                                <div key={index}>
-                                    <div>{picture.picture}</div>
-                                    <span>{picture.caption}</span>
-                                </div>
-                            ))}
-                        </div>
-                        <div>
-                            {currentQuestion.options.map((option, index) => (
-                                <div key={index}>{option.heading}</div>
-                            ))}
-                        </div>
-                    </div>
+                    {questionList.map((question,index)=>{
+                        // const [currentQuestion,setCurrentQuestion] = useState({
+                        //     name:'',
+                        //     text:'',
+                        //     pictures:[],
+                        //     options:[]
+                        // })
+                        return(
+                            <div onClick={(question)=>prepareTest(question)} key={index}>
+                                {question.name}
+                                {/*<div>*/}
+                                {/*    <span onClick={()=>{getRequiredQuestion(question,setCurrentQuestion)}}>Запросить вопрос</span>*/}
+                                {/*    <div>*/}
+                                {/*        <span>{currentQuestion.name}</span>*/}
+                                {/*        <span>{currentQuestion.text}</span>*/}
+                                {/*        <div>*/}
+                                {/*            {currentQuestion.pictures.map((picture,index)=>{*/}
+                                {/*                return(*/}
+                                {/*                    <div>*/}
+                                {/*                        <div>*/}
+                                {/*                            {picture.picture}*/}
+                                {/*                        </div>*/}
+                                {/*                        <span>{picture.caption}</span>*/}
+                                {/*                    </div>*/}
+                                {/*                )*/}
+                                {/*            })}*/}
+                                {/*        </div>*/}
+                                {/*        <div>*/}
+                                {/*            {currentQuestion.options.map((option,index)=>{*/}
+                                {/*                return(*/}
+                                {/*                    <div>*/}
+                                {/*                        {option.heading}*/}
+                                {/*                    </div>*/}
+                                {/*                )*/}
+                                {/*            })}*/}
+                                {/*        </div>*/}
+                                {/*    </div>*/}
+                                {/*</div>*/}
+                            </div>
+                        )
+                    })
+                    }
                 </div>
-            </div>
-        );
-    };
+            )
+        }
+    }
 
+    const prepareTest = (question) => {
+        if(testList.includes(question))
+            testList.push(question)
+        else setTestList(testList.filter((quest,index)=>quest!==question))
+    }
     const NameInput = () => {
         const [name, setName] = useState('');
 
@@ -113,9 +117,9 @@ const TestConst = () => {
 
         return (
             <div className="qc__modal">
-        <span className="qc__xclose" onClick={() => setIsReady(false)}>
-          Закрыть
-        </span>
+                <span className="qc__xclose" onClick={() => setIsReady(false)}>
+                    Закрыть
+                </span>
                 <form onSubmit={handleSubmit}>
                     <input
                         className="qc__input"
@@ -123,33 +127,23 @@ const TestConst = () => {
                         onChange={(e) => setName(e.target.value)}
                         type="text"
                     />
-                    <button
-                        className="qi__button"
-                        type="button"
-                        onClick={() => {
-                            sendTest(name, testList);
-                        }}
-                    >
+                    <button className="qi__button" type="button" onClick={()=>{
+                        sendTest(name,testList)
+                    }
+                    }>
                         Загрузить тест на сервер
                     </button>
                 </form>
             </div>
         );
     };
-
     return (
         <div>
             <Header />
             <div style={{ display: 'flex' }}>
                 <DbSideBar />
                 <Body>
-                    {questionList && (
-                        <div>
-                            {questionList.map((question, index) => (
-                                <QuestionItem key={index} question={question} />
-                            ))}
-                        </div>
-                    )}
+                    <PrintQuestionList/>
                     <button className="qi__button" onClick={() => setIsReady(true)}>
                         Завершить формирование теста
                     </button>
